54e9ce34d5275fc8dc9a9458268c97c8512d12ce
ghd9201 <ghd9201@naver.com>
2019-11-28 03:00:42 +0000

    have to resolve npm run dev error

--- fchain/server/routes.js
+++ fchain/server/routes.js
@@ -3,5 +3,6 @@ import fchainRouter from './api/controllers/fchain/router';
 
 export default function routes(app) {
   app.use('/api/v1/examples', examplesRouter);
-  app.use('/api/v1/fchain', balanceRouter);
+  app.use('/api/v1/fchain', fchainRouter);
+	
 }
